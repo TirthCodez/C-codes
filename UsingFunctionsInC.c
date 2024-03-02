c32d5629b8034d1c819afde1869f56507098a54a
TirthCodez <tirthpopat1722@gmail.com>
2024-03-02 21:09:55 +0530

    Using function in C language

---
+++ UsingFunctionsInC.c
@@ -0,0 +1,15 @@
+//Make a function to print hello world
+#include<stdio.h>
+
+//Declaration of function
+void helloworld();
+
+int main(){
+    helloworld(); //calling of a function
+    return 0;
+}
+
+//definition of function or also called as statements in function
+void helloworld(){
+    printf("Hello World");
+}
